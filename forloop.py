1582be10e7b6fff164fd8b775d3c03efe62d19df
Sony Eda <sonyeda601@gmail.com>
2026-09-20 18:53:29 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,31 @@
+numbers = [12, 5, 27, 8, 19]
+num = sum(numbers)
+print(num)
+
+# largest = numbers[0]
+# for num in numbers:
+#   if num > largest:
+#     largest = num 
+# print (largest)
+
+# smallest = numbers[0]
+# for num in numbers :
+#   if num < smallest:
+#     smallest = num
+# print(smallest)
+
+# append , insert , remove,  pop, len 
+
+a = [1,2,3,5,7,6,7,8,9]
+# total = len(a)
+# print(total)
+
+# a.remove(1)
+# print (a)
+
+# a.reverse()
+# print(a)
+
+# sort 
+a.sort()
+print(a)
